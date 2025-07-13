ab42c43693062b7c4077471137e05ec552028146
artik0601 <artikri0601@gmail.com>
2025-07-13 16:40:42 +0530

    Use LandingPage directly as homepage

--- app/page.jsx
+++ app/page.jsx
@@ -9,17 +9,26 @@
 //   }, [router]);
 //   return null;
 // }
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 
-export default function HomeRedirect() {
-  const router = useRouter();
+// export default function HomeRedirect() {
+//   const router = useRouter();
 
-  useEffect(() => {
-    router.replace("/landing");
-  }, [router]);
+//   useEffect(() => {
+//     router.replace("/landing");
+//   }, [router]);
 
-  return null;
+//   return null;
+// }
+
+
+'use client';
+
+import LandingPage from './landing/page';
+
+export default function Home() {
+  return <LandingPage />;
 }
